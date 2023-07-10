3f26564feca984f30614758e0186c14e753e25b0
phamnam91th <phamnam1991@gmail.com>
2023-07-11 00:03:01 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module vn.aptech.project_k2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens vn.aptech.project_k2 to javafx.fxml;
+    exports vn.aptech.project_k2;
+}
